f20b9438da4a29f84c7a225d96e010db925ee69d
Yulon <yulon@me.com>
2022-09-15 00:12:07 +0800

    feat: Dial() return handshake RTT

--- client.go
+++ client.go
@@ -19,12 +19,12 @@ func Dial(addr, user, pass string) (*Conn, time.Duration, error) {
 	if err != nil {
 		return nil, -1, err
 	}
-	frt, err := con.sendHandshake(user, pass)
+	rtt, err := con.sendHandshake(user, pass)
 	if err != nil {
 		con.Close()
-		return nil, frt, err
+		return nil, rtt, err
 	}
-	return con, frt, nil
+	return con, rtt, nil
 }
 
 var ErrorNotSuportedGSSAPI = errors.New("not suported GSSAPI")
@@ -78,14 +78,14 @@ func (con *Conn) sendHandshake(user, pass string) (time.Duration, error) {
 		return -1, err
 	}
 
-	frt := time.Now().Sub(now)
+	rtt := time.Now().Sub(now)
 
 	switch buf[1] {
 	case MethodNone:
-		return frt, nil
+		return rtt, nil
 
 	case MethodGSSAPI:
-		return frt, ErrorNotSuportedGSSAPI
+		return rtt, ErrorNotSuportedGSSAPI
 
 	case MethodUsernamePassword:
 		/*
@@ -113,7 +113,7 @@ func (con *Conn) sendHandshake(user, pass string) (time.Duration, error) {
 
 		err := writeAll(con, buf)
 		if err != nil {
-			return frt, err
+			return rtt, err
 		}
 
 		/*
@@ -126,18 +126,18 @@ func (con *Conn) sendHandshake(user, pass string) (time.Duration, error) {
 
 		err = readFull(con, buf[:2])
 		if err != nil {
-			return frt, err
+			return rtt, err
 		}
 
 		if buf[1] != UserPassStatusSuccess {
-			return frt, ErrorAuthenticationFailed
+			return rtt, ErrorAuthenticationFailed
 		}
-		return frt, nil
+		return rtt, nil
 
 	case MethodUnsupportAll:
-		return frt, ErrorNoMethodAvailabled
+		return rtt, ErrorNoMethodAvailabled
 	}
-	return frt, ErrorUnknowMethod
+	return rtt, ErrorUnknowMethod
 }
 
 var ErrorServerFailure = errors.New("server failure")
@@ -150,89 +150,95 @@ var ErrorCommandNotSupported = errors.New("request command not supported")
 var ErrorAddressNotSupported = errors.New("address not supported")
 var ErrorUnknowReply = errors.New("unknow reply")
 
-func (con *Conn) Command(cmd byte, addr net.Addr) (net.Addr, error) {
+func (con *Conn) Command(cmd byte, addr net.Addr) (net.Addr, time.Duration, error) {
+	now := time.Now()
+
 	err := con.WriteAddr(cmd, addr)
 	if err != nil {
-		return nil, err
+		return nil, -1, err
 	}
+
 	rep, addr, err := con.ReadAddr()
 	if err != nil {
-		return nil, err
+		return nil, -1, err
 	}
+
+	rtt := time.Now().Sub(now)
+
 	if rep != RepSuccess {
 		switch rep {
 		case RepServerFailure:
-			return nil, ErrorServerFailure
+			return nil, rtt, ErrorServerFailure
 		case RepNotAllowed:
-			return nil, ErrorNotAllowed
+			return nil, rtt, ErrorNotAllowed
 		case RepNetworkUnreachable:
-			return nil, ErrorNetworkUnreachable
+			return nil, rtt, ErrorNetworkUnreachable
 		case RepHostUnreachable:
-			return nil, ErrorHostUnreachable
+			return nil, rtt, ErrorHostUnreachable
 		case RepConnectionRefused:
-			return nil, ErrorConnectionRefused
+			return nil, rtt, ErrorConnectionRefused
 		case RepTTLExpired:
-			return nil, ErrorTTLExpired
+			return nil, rtt, ErrorTTLExpired
 		case RepCommandNotSupported:
-			return nil, ErrorCommandNotSupported
+			return nil, rtt, ErrorCommandNotSupported
 		case RepAddressNotSupported:
-			return nil, ErrorAddressNotSupported
+			return nil, rtt, ErrorAddressNotSupported
 		}
-		return nil, ErrorUnknowReply
+		return nil, rtt, ErrorUnknowReply
 	}
-	return addr, nil
+	return addr, rtt, nil
 }
 
-func (con *Conn) DialTCP(addr net.Addr) (net.Conn, error) {
-	_, err := con.Command(CmdConnect, addr)
+func (con *Conn) DialTCP(addr net.Addr) (net.Conn, time.Duration, error) {
+	_, rtt, err := con.Command(CmdConnect, addr)
 	if err != nil {
-		return nil, err
+		return nil, rtt, err
 	}
-	return con.Conn, nil
+	return con.Conn, rtt, nil
 }
 
-func DialTCP(server, user, pass string, dest net.Addr) (net.Conn, time.Duration, error) {
-	con, frt, err := Dial(server, user, pass)
+func DialTCP(server, user, pass string, dst net.Addr) (net.Conn, time.Duration, time.Duration, error) {
+	con, pxyRtt, err := Dial(server, user, pass)
 	if err != nil {
-		return nil, frt, err
+		return nil, pxyRtt, -1, err
 	}
-	tcpCon, err := con.DialTCP(dest)
+	tcpCon, dstRtt, err := con.DialTCP(dst)
 	if err != nil {
 		con.Close()
-		return nil, frt, err
+		return nil, pxyRtt, dstRtt, err
 	}
-	return tcpCon, frt, err
+	return tcpCon, pxyRtt, dstRtt, err
 }
 
-func (con *Conn) ListenUDP() (net.PacketConn, error) {
-	udpPxyAddr, err := con.Command(CmdUDP, &net.UDPAddr{IP: net.IPv4zero, Port: 0})
+func (con *Conn) ListenUDP() (net.PacketConn, time.Duration, error) {
+	udpPxyAddr, rtt, err := con.Command(CmdUDP, &net.UDPAddr{IP: net.IPv4zero, Port: 0})
 	if err != nil {
-		return nil, err
+		return nil, rtt, err
 	}
 	udpPxyPubAddr, err := con.ResolveRemoteAddr(udpPxyAddr)
 	if err != nil {
-		return nil, err
+		return nil, rtt, err
 	}
 	udpPxyPubUDPAddr, err := AddrToUDP(udpPxyPubAddr)
 	if err != nil {
-		return nil, err
+		return nil, rtt, err
 	}
 	udpPxyUDPCon, err := net.DialUDP("udp", nil, udpPxyPubUDPAddr)
 	if err != nil {
-		return nil, err
+		return nil, rtt, err
 	}
-	return &udpConn{con.Conn, udpPxyUDPCon}, nil
+	return &udpConn{con.Conn, udpPxyUDPCon}, rtt, nil
 }
 
-func ListenUDP(server, user, pass string) (net.PacketConn, time.Duration, error) {
-	con, frt, err := Dial(server, user, pass)
+func ListenUDP(server, user, pass string) (net.PacketConn, time.Duration, time.Duration, error) {
+	con, pxyRtt, err := Dial(server, user, pass)
 	if err != nil {
-		return nil, frt, err
+		return nil, pxyRtt, -1, err
 	}
-	udpCon, err := con.ListenUDP()
+	udpCon, dstRtt, err := con.ListenUDP()
 	if err != nil {
 		con.Close()
-		return nil, frt, err
+		return nil, pxyRtt, dstRtt, err
 	}
-	return udpCon, frt, err
+	return udpCon, pxyRtt, dstRtt, err
 }
